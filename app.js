ac97a514807ed6ad829bf2665e0d1f89fa626e7e
Jenn Henderson <jenn.henderson18@gmail.com>
2019-08-03 16:32:21 -0700

    train input storing to firebase

--- app.js
+++ app.js
@@ -1,11 +1,60 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBPuPWjMa6yEJnFt6hvlmb82vfpTmY3ahI",
+    authDomain: "train-scheduler-3ade6.firebaseapp.com",
+    databaseURL: "https://train-scheduler-3ade6.firebaseio.com",
+    projectId: "train-scheduler-3ade6",
+    storageBucket: "",
+    messagingSenderId: "811935117326",
+    appId: "1:811935117326:web:239406a0399973b6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  //create variable for access Firebase
+  var database = firebase.database();
+
 // create an onclick function to adding a new train
 
-// store user input into variables
+$('#add-train-btn').on('click', function(event) {
+    event.preventDefault();
 
-// clear the form
+    // store user input into variables
+    var trainName = $('#train-name-input').val().trim();
+    var trainDestination = $('#destination-input').val().trim();
+    var firstTrainTime = $('#first-train-input').val().trim();
+    var trainFrequency = $('#frequency-input').val().trim();
 
-// use the variables to dynamically add information to the table
-    // what is static
-    // what needs to be calculated?
+    // clear the form
+    $('#train-name-input').val('');
+    $('#destination-input').val('');
+    $('#first-train-input').val('');
+    $('#frequency-input').val('');
+
+    // create local "temporary" object for holding the train data
+    var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        firstTrain: firstTrainTime,
+        frequency: trainFrequency
+    };
+
+    //store information in Firebase
+    database.ref().push(newTrain);
+
+    //display information using Firebase
+
+    
 
 
+
+
+
+
+})
+
+
+
+// ================== BACKLOG =============
+
+// add a trash can icon that removes a talbe row and the data stored from Firebase
