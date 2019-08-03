3cdfc1f277f93ce7eac7af5968f78bccf4940c6d
Jenn Henderson <jenn.henderson18@gmail.com>
2019-08-03 16:08:53 -0700

    adding html

---
+++ app.js
@@ -0,0 +1,11 @@
+// create an onclick function to adding a new train
+
+// store user input into variables
+
+// clear the form
+
+// use the variables to dynamically add information to the table
+    // what is static
+    // what needs to be calculated?
+
+
